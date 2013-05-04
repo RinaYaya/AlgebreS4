e65c056ec99785febdb056fab826ec028fde5865
RinaYaya <yacine.oleemahomed@club-internet.fr>
2013-05-05 01:52:12 +0200

    dza

--- main.c
+++ main.c
@@ -73,7 +73,7 @@ int iVar=0, iMat=0, Ligne=0, Col=0, iCptL=0, iCptC=0, iT=0,i=0;
 					i++;
 				}
 				iMat++;
-				for(iCptL=0; iCptL <Ligne; iCptL)
+				for(iCptL=0; iCptL <Ligne; iCptL++)
 				{
 					for(iCptC=0; iCptC<Col; iCptC++)
 					{
