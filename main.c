32e5af36dfb8bc659149ac599be007aecc19d1da
RinaYaya <yacine.oleemahomed@club-internet.fr>
2013-05-05 00:44:18 +0200

    atof pas atoi

--- main.c
+++ main.c
@@ -68,7 +68,7 @@ char s1[50];
 									else
 										if((s2[0] >= '0')&&(s2[0] <= '9'))
 										{
-											TabNombre[iVar]=NewNombre(s1,atoi(s2));
+											TabNombre[iVar]=NewNombre(s1,atof(s2));
 											printf(" %s = %d \n",TabNombre[iVar]->variable,TabNombre[iVar]->valeur);
 										}
 										else
