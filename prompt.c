44285cdf30947a1b6205980eecd327ea07fa4ab7
RinaYaya <yacine.oleemahomed@club-internet.fr>
2013-05-04 13:11:10 +0200

    Ajout prompt.h prompt.c

--- prompt.c
+++ prompt.c
@@ -1,27 +1,17 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
+#include "prompt.h"
+
 //----------------------------------------------------------------------
+/**
+ * \fn void LireChaine(char chaine[], int size) 
+ * \brief Lit une chaine et la met dans chaine []
+ * \param size taille de chaine[]
+ * \param chaine[] chaine de caractere
+ * \return void
+ *  \pre 
+ */
 void LireChaine(char chaine[], int size) 
 {  
     fgets(chaine, size, stdin);  
     chaine[strlen(chaine) - 1] = '\0';  
-}  
-
-int main ()
-{
-
-char s[50];
-int i=0;
-
-	while(1)
-	{
-		printf("> ");
-		LireChaine(s,50);
-		printf("%s \n",s);
-	}
-
-  return 0;
-}
+} 
 //----------------------------------------------------------------------
---
+++ prompt.h
@@ -0,0 +1,15 @@
+/**
+* \file prompt.h
+* \brief  Fonction qui gere le prompt
+*/
+
+#ifndef _SOMMET_H__
+#define _SOMMET_H__
+//-----------------------------Include----------------------------------
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+//-----------------------------Prototype--------------------------------
+void LireChaine(char chaine[], int size);
+#endif
