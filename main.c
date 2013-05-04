990c22ef0b3e434cbb56ae29a086cb6ae29744db
RinaYaya <yacine.oleemahomed@club-internet.fr>
2013-05-04 23:14:44 +0200

    fa

--- main.c
+++ main.c
@@ -9,24 +9,29 @@
 int main ()
 {
 
+int iQuit =0;
 char s[50];
 char s2[50];
 char s1[50];
-	
+
+	while(iQuit == 0)
+	{	
 		printf("> ");
 		LireChaine(s);
+		Argument(s,s2,0);
+		Argument(s,s1,1);
+		
+		if(strcmp(s1,"quit") ==0)
+		{
+			iQuit=1;
+		}
+	}
 
 
 
-	Argument(s,s2,0);
-	Argument(s,s1,1);
+
 	printf(" %s | %s \n",s2,s1);
-	//~ switch (s2)
-	//~ {
-		//~ case "a":
-				//~ break;
-				//~ 
-	//~ }
+
 
   return 0;
 }
