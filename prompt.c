b98b485fd9c51b02b8be68b683dee2c1e5b9fb3d
RinaYaya <yacine.oleemahomed@club-internet.fr>
2013-05-04 15:26:15 +0200

    Argument peut recupéré avant : et apres :

--- prompt.c
+++ prompt.c
@@ -19,18 +19,39 @@ void Argument(char chaine1[],char chaine2[],int choix)
 {
 	int i=0;
 	
-	if(choix == 1)
+	switch (choix)
 	{
-		while((i<4)&&(chaine1[i] != ':'))
-		{
-			if(chaine1[i] != ' ')
-			{
-				chaine2[i]=chaine1[i];
-			}
-			i++;
-		}
-		chaine2[i]='\0';
+		case '1':	while((i<N)&&(chaine1[i] != ':'))
+					{
+						if(chaine1[i] != ' ')
+						{
+							chaine2[i]=chaine1[i];
+						}
+						i++;
+					}
+					chaine2[i]='\0';
+					break;
+					
+		case '2':	while((i<N)&&(chaine1[i] != ':'))
+					{
+						i++;
+					}
+					
+					while((i<N)&&(chaine1[i] != '('))
+					{
+						if(chaine1[i] != ' ')
+						{
+							chaine2[i]=chaine1[i];
+						}
+						i++;
+					}
+					chaine2[i]='\0';
+					break;
+					
+
 	}
+	
+	if
 	
 	
 }
