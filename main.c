215aae5e7b6622705da542c4e57478b7bcdf07b3
RinaYaya <yacine.oleemahomed@club-internet.fr>
2013-05-04 12:38:23 +0200

    Saisie clavié ok

--- main.c
+++ main.c
@@ -6,9 +6,16 @@ int main ()
 {
 
 char s[50];
-
+int i=0;
 	while(1)
 	{
+		i=0;
+		//on vide le buffer
+		while(s[i] != '\0')
+		{
+			i++;
+		}
+		
 		printf("> ");
 		scanf("%s ",s);
 		printf("%s \n",s);
