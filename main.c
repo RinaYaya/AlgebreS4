430614409ac6fc7dbf7579d3c4eeb78d34abb34d
RinaYaya <yacine.oleemahomed@club-internet.fr>
2013-05-05 23:12:22 +0200

    dzadza

--- main.c
+++ main.c
@@ -162,7 +162,7 @@ int iVar=0, iMat=0, Ligne=0, Col=0, iCptL=0, iCptC=0, iT=0,i=0,ouvert=0, imult=0
 					 
 					
 					TabMat[iMat]=NewMatrice(s1,TabMat[i1]->matrice->nrows,TabMat[i2]->matrice->ncols );
-					printf(" RO %d CO %d \n",TabMat[i1]->matrice->nrows,TabMat[i2]->matrice->ncols);
+					printf("%s: %d §s : %d \n",TabMat[i1]->nom,TabMat[i1]->matrice->nrows,TabMat[i2]->nom,TabMat[i2]->matrice->ncols);
 					strcpy(TabMat[iMat]->nom, s1);
 					TabMat[iMat]->matrice=multiplication(TabMat[i1]->matrice,TabMat[i2]->matrice);
 					
