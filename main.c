3c5458c806e03bf920217347d1acb8fe14c5ba17
RinaYaya <yacine.oleemahomed@club-internet.fr>
2013-05-05 14:57:55 +0200

    daz

--- main.c
+++ main.c
@@ -116,7 +116,7 @@ int iVar=0, iMat=0, Ligne=0, Col=0, iCptL=0, iCptC=0, iT=0,i=0,ouvert=0;
 				{
 					for(iCptC=0; iCptC<Col; iCptC++)
 					{
-						printf("m[ %d ][ %d ] = %f ",iCptL,iCptC,getElt(TabMat[iMat-1]->matrice, iCptL, iCptC));
+						printf("m[ %d ][ %d ] = %f \n",iCptL,iCptC,getElt(TabMat[iMat-1]->matrice, iCptL, iCptC));
 					}
 					
 					printf("\n");
