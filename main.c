9ab5d06509857cffaac987e61231d9442b4b2ed3
RinaYaya <yacine.oleemahomed@club-internet.fr>
2013-05-05 18:27:17 +0200

    cz

--- main.c
+++ main.c
@@ -87,7 +87,6 @@ int iVar=0, iMat=0, Ligne=0, Col=0, iCptL=0, iCptC=0, iT=0,i=0,ouvert=0;
 					}
 					
 
-					
 					if(((s[i]>='0')&&(s[i]<='9')))
 					{
 						printf(" 	Entrer dans 0-9 \n");
@@ -95,7 +94,7 @@ int iVar=0, iMat=0, Ligne=0, Col=0, iCptL=0, iCptC=0, iT=0,i=0,ouvert=0;
 						iT++;
 					}
 					
-					if(s[i] == ',')
+					if((s[i] == ',')||(s[i] == ']'))
 					{
 						//~ printf(" 	Entrer dans , ] \n");
 						Temp[iT]='\0';
