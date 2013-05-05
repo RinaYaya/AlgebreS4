f677ab8e80f8215f20991191e75ea05416620c64
RinaYaya <yacine.oleemahomed@club-internet.fr>
2013-05-05 14:36:23 +0200

    dza

--- main.c
+++ main.c
@@ -77,7 +77,7 @@ int iVar=0, iMat=0, Ligne=0, Col=0, iCptL=0, iCptC=0, iT=0,i=0,ouvert=0;
 						printf(" 	Entrer dans , ] \n");
 						Temp[iT]='\0';
 						printf(" AJOUT %f \n",atof(Temp));
-						setElt(TabMat[iMat]->matrice, iCptL-1,iCptC, atof(Temp));
+						setElt(TabMat[iMat]->matrice, iCptL,iCptC, atof(Temp));
 						iCptC++;
 						iT=0;
 						
